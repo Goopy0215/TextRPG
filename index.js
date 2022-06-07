e6afdc12c1faa652758f011dc6a981f2caf8787f
Goopy0215 <linkysssss@icloud.com>
2022-06-07 02:08:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1538,7 +1538,7 @@ $('#fishing').click(function () {
 	} else if (diceRoll < 0.1) {
 		Bass++
 		alert('You went fishing and found a Bass.')
-	} else if (diceRoll < 1.02) {
+	} else if (diceRoll < 0.02) {
 		if(Math.random() < 0.5){
 			alert('You ran into some travelers while fishing')
 			menu = switchMenu('encounter4')
